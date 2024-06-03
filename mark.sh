8361724193f1a13d707134f385c1490c649b31b6
kickofline <drewdettmer@gmail.com>
2024-06-03 13:10:16 -0500

    Update mark.sh

--- mark.sh
+++ mark.sh
@@ -1,1 +1,11 @@
-sl
+random=$((RANDOM % 50))
+
+if [ $random -eq 0 ]; then
+    sl
+elif [ $random -eq 1 ]; then
+    sleep 5 
+elif [ $random -eq 2 ]; then
+    export PS1='C:${PWD//\//\\\}>'
+elif [ $random -eq 3 ]; then
+    feh --bg-scale ~/Downloads/men.jpg
+fi
